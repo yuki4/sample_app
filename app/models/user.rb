f6df5156678a5a6abd1122ee01e41e24da6c6933
yuki <b1434514@planet.kanazawa-it.ac.jp>
2015-09-25 13:11:23 +0900

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :microposts, dependent: :destroy
   has_secure_password
   before_save { email.downcase! }
   before_save { self.email = email.downcase }
@@ -18,6 +19,10 @@ class User < ActiveRecord::Base
 
   def User.encrypt(token)
     Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  def feed
+    Micropost.where("user_id = ?", id)
   end
 
   private
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -14,7 +14,9 @@ describe "User pages" do
     end
 
     it { should have_title('All users') }
-    it { should have_content('All users') }
+
+    describe "microposts" do
+      it { should have_content('All users') }
 
     describe "pagination" do
 
@@ -50,14 +52,22 @@ describe "User pages" do
         it { should_not have_link('delete', href: user_path(admin)) }
       end
     end
-  end
+    end
 
-  describe "profile page" do
-    let(:user) { FactoryGirl.create(:user) }
-    before { visit user_path(user) }
+    describe "profile page" do
+      let(:user) { FactoryGirl.create(:user) }
+      let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
+      let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") }
 
-    it { should have_content(user.name) }
-    it { should have_title(user.name) }
+      before { visit user_path(user) }
+
+      it { should have_content(user.name) }
+      it { should have_title(user.name) }
+
+      it { should have_content(m1.content) }
+      it { should have_content(m2.content) }
+      it { should have_content(user.microposts.count) }
+    end
   end
 
 	describe "signup page" do
